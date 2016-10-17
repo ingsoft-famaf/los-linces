2e73342091237c0d97aed6034c727a8183a09a81
alki22 <alki.collias@gmail.com>
2016-10-17 19:50:53 -0300

    Added video playing URL

--- VideoChat/VideoPlay/urls.py
+++ VideoChat/VideoPlay/urls.py
@@ -3,5 +3,6 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-url(r'^$', TemplateView.as_view(template_name="reproductor.html")),
+	# Example : /VideoPlay/35
+	url(r'^(?P<video_id>[0-9]+)/$', TemplateView.as_view(template_name="reproductor.html"), name='play'),
 ]
