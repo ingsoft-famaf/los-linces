28f4d9d47ac2c0c3e6c68606d074788ed6594c7c
Elías Gasparini <gasparinielias@gmail.com>
2016-10-20 12:14:39 -0300

    Bug fix. Static files are shared from Site app. Now player reproduces videos.

--- VideoChatProject/Site/urls.py
+++ VideoChatProject/Site/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^upload/', include('uploader.urls')),
     url(r'^search/', include('haystack.urls')),
     url(r'^play/', include('VideoChat.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
