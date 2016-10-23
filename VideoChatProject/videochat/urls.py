97cb6201c2234ab1b48bcee8c956f73a7633b888
aguilarjp <aguilarjp.95@gmail.com>
2016-10-23 20:25:54 -0300

    Link video results with player

--- VideoChatProject/videochat/urls.py
+++ VideoChatProject/videochat/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'videochat'
+
 urlpatterns = [
     # ... the rest of your URLconf goes here ...
     url(r'^(?P<video_id>[0-9]+)/$', views.play, name='play'),
