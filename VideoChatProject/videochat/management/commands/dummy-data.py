5154d8441c08554c6c894af9f7951f9f3f8e6826
aguilarjp <aguilarjp.95@gmail.com>
2016-10-23 20:13:27 -0300

    Improve dummy-data command

--- VideoChatProject/videochat/management/commands/dummy-data.py
+++ VideoChatProject/videochat/management/commands/dummy-data.py
@@ -2,28 +2,43 @@ from model_mommy import mommy
 from django.core.management.base import BaseCommand
 from . import markovgen
 import os
-from .models import Video
+from haystack.management.commands import update_index
+from videochat.models import Video
 
 
 class Command(BaseCommand):
     videos = []
 
     def add_arguments(self, parser):
+        parser.add_argument('action', nargs=1, type=str)
         parser.add_argument('count', nargs=1, type=int)
 
     def handle(self, *args, **options):
-        print("Creating dummy data...")
-        self.make_videos(options)
+        if options.get('action')[0] == 'create':
+            print("Creating dummy data...")
+            self.make_videos(options)
+            update_index.Command().handle()
+
+        if options.get('action')[0] == 'clear':
+            print("Deleting dummy data...")
+            self.clear()
+
+        if options.get('action')[0] == 'count':
+            self.count()
 
     def make_videos(self, options):
         f = open(os.path.abspath("videochat/management/commands/source"))
         markov = markovgen.Markov(f)
-        for _ in range(options.get('count')[0]):
-            video_title = markov.generate_markov_text(10)
-            video_description = markov.generate_markov_text(200)
+        for i in range(options.get('count')[0]):
+            # video_title = markov.generate_markov_text(10)
+            video_title = "sample " + str(i)
+            video_description = markov.generate_markov_text(100)
             video = mommy.make(Video, title=video_title, description=video_description)
 
             self.videos.append(video)
 
     def clear(self):
         Video.objects.all().delete()
+
+    def count(self):
+        print("Count: " + str(Video.objects.all().count()))
