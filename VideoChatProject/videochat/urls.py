7b4fb147aa77ea769ef32c6500d9aa1743a0b368
aguilarjp <aguilarjp.95@gmail.com>
2016-10-23 16:28:22 -0300

    Fix bug in search bar form

--- VideoChatProject/videochat/urls.py
+++ VideoChatProject/videochat/urls.py
@@ -1,9 +1,7 @@
-from django.conf import settings
 from django.conf.urls import url
-from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     # ... the rest of your URLconf goes here ...
-	url(r'^(?P<video_id>[0-9]+)/$', views.play, name='play'),
+    url(r'^(?P<video_id>[0-9]+)/$', views.play, name='play'),
 ]
