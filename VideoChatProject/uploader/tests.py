3bcbff27c295fbbeb34d447c9dea87e2b1b18dd6
Male <marianelamoralese@gmail.com>
2016-10-24 19:23:53 -0300

    Automated Testing for Uploader: Redirect when not logged in

--- VideoChatProject/uploader/tests.py
+++ VideoChatProject/uploader/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class UserRedirectTest(TestCase):
+
+    def test_index_redirect_when_not_logged_in(self):
+        client = Client()
+        response = client.get(reverse('index'))
+        self.assertIs(response.status_code, 302)
+        self.assertContains(response.url, 'login')
