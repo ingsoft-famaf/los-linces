217c4e69c7885af092156f7a2966bef498b5681e
Male <marianelamoralese@gmail.com>
2016-10-24 18:59:03 -0300

    Bug fix in automated testing of Video lay: Add one to last pk assigned

--- VideoChatProject/videochat/tests.py
+++ VideoChatProject/videochat/tests.py
@@ -1,18 +1,22 @@
 from django.test import TestCase
 from .models import Video
 from django.test.client import RequestFactory
+from django.contrib.auth.models import User
 
 class VideoPlayMethodTest(TestCase):
+
 	
 	def _404_tests(self):
 
 		request_factory = RequestFactory()
-		url = 'http://localhost:8000/play/v/999999'
+		#user = User.objects.create_user(username="pepita", password="contraseñadificil")
+		#authenticate(username=user.username, password=user.password)
+		video = Video(title= "Primer video", description= "probando", path="/media/videos/2016/10/20/sample4")
+		video.save()
+		videopk = video.pk
+		url = 'http://localhost:8000/play/v/videopk'
 		request = request_factory.get(url)
-		video_id = 999999 # Test: Video_id doesn't exist
-		response = play(request, video_id)
-		self.assertIs(video_id._404_tests,404)
+		response = play(request, videopk)
+		self.assertIs(videopk,404)
 
 	
-
-
