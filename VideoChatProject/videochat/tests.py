5a4e2607d7f2ec67795d3bd82d30a3aca082e3b7
Male <marianelamoralese@gmail.com>
2016-10-24 17:16:02 -0300

    Automated Testing for VideoPlay: Add test that checks if the view returns 404 when a particular video doesn't exist. This is the success case. Otherwise, the test fails.

--- VideoChatProject/videochat/tests.py
+++ VideoChatProject/videochat/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Video
+from django.test.client import RequestFactory
 
-# Create your tests here.
+class VideoPlayMethodTest(TestCase):
+	
+	def _404_tests(self):
+
+		request_factory = RequestFactory()
+		url = 'http://localhost:8000/play/v/999999'
+		request = request_factory.get(url)
+		video_id = 999999 # Test: Video_id doesn't exist
+		response = play(request, video_id)
+		self.assertIs(video_id._404_tests,404)
+
+	
+
+
