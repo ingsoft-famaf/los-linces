131f90303228e64306ac0b1f0480aae5d67100ae
alki22 <alki.collias@gmail.com>
2016-10-25 17:18:44 -0300

    Added chat message model

--- VideoChatProject/videochat/models.py
+++ VideoChatProject/videochat/models.py
@@ -22,3 +22,20 @@ class Video(models.Model):
 
         return '\n'.join([title, description, path, pub_date])
 
+class Message(models.Model):
+	text = models.TextField()
+	date_sent = models.DateTimeField(auto_now=True)
+	author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null = 'True'
+    )
+	video = models.ForeignKey(
+		Video,
+		on_delete=models.CASCADE
+	)
+
+	def __str__(self):
+		date_sent = 'Sent on: ' + str(self.date_sent)
+		text = 'Text: ' + str(self.text)
+		return '\n'.join([date_sent, text])
