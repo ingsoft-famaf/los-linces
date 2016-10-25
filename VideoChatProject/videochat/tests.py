c0573a29062f45db7d3f65d4edd9c3b1edd4b335
Male <marianelamoralese@gmail.com>
2016-10-25 18:56:44 -0300

    Advance test for Video Play

--- VideoChatProject/videochat/tests.py
+++ VideoChatProject/videochat/tests.py
@@ -2,30 +2,36 @@ from django.test import TestCase
 from .models import Video
 from django.test.client import RequestFactory
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test import Client
 
 class VideoPlayMethodTest(TestCase):
 
-	
-	def _404_tests(self):
+	def setUp(self):
+		self.video = Video(title= "Primer video", description= "probando", path="/media/videos/2016/10/20/sample4")
+		self.video.save()
+		self.videopk = self.video.pk
+		self.client = Client()
+
+	def test_404(self):
 
 		request_factory = RequestFactory()
 		#user = User.objects.create_user(username="pepita", password="contraseñadificil")
 		#authenticate(username=user.username, password=user.password)
-		video = Video(title= "Primer video", description= "probando", path="/media/videos/2016/10/20/sample4")
-		video.save()
-		videopk = video.pk + 1
-		url = 'http://localhost:8000/play/v/videopk'
+		self.videopk = self.video.pk + 1
+		url = self.client.get(reverse('videochat: v'))
 		request = request_factory.get(url)
-		response = play(request, videopk)
-		self.assertIs(response.status_code,404)
+		response = v(request, videopk)
+		self.assertEqual(response.status_code,404)
 
 	def _202_tests(self):
 		
 		request_factory = RequestFactory()
 		url = 'http://localhost:8000/play/v/videopk'
 		request = request_factory.get(url)
-		video = Video(title= "Primer video", description= "probando", path="/media/videos/2016/10/20/sample4")
-		video.save()
-		videopk = video.pk
+		self.videopk = self.video.pk
 		#response = play(request, videopk)
-		self.assertIs(response.status_code,202)
+		self.assertEqual(response.status_code,202)
+
+	#def check_template_used():
+		
