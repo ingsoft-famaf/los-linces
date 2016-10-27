641a7e0f1a4d10c37a95b6789ea19ddedc2c4066
Elías Gasparini <gasparinielias@gmail.com>
2016-10-27 12:38:34 -0300

    Uploader: fix 1 test. Add test with authenticated user

--- VideoChatProject/uploader/tests.py
+++ VideoChatProject/uploader/tests.py
@@ -1,14 +1,30 @@
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 from django.test.utils import setup_test_environment
 
 
-class UploadTest(TestCase):
+class NonAuthenticatedUserTest(TestCase):
     def setUp(self):
         self.client = Client()
 
     def test_index_redirect_when_not_logged_in(self):
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('uploader:index'))
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('login') + '?next=/')
+        self.assertRedirects(response, reverse('login') + '?next=/upload/')
+
+
+class AuthenticatedUserTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        username = "TestUser"
+        password = "TestPassword"
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        self.client.login(username=username, password=password)
+
+    def test_index_when_logged_in(self):
+        response = self.client.get(reverse('uploader:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, reverse('uploader:index'))
