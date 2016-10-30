20bb610809a5e11fc22bc4c8efa2f98607a8630c
aguilarjp <aguilarjp.95@gmail.com>
2016-10-30 05:17:51 -0300

    Fix button bug: Now app checks whether a frienship request was previously sent or not

--- VideoChatProject/videochat/ajax.py
+++ VideoChatProject/videochat/ajax.py
@@ -13,7 +13,20 @@ def add_friend(request):
         to_user = User.objects.get(pk=request.POST.get('to_user'))
 
         Friend.objects.add_friend(from_user, to_user)
-        data = {'message': "%s added" % request.POST.get('to_user')}
+        data = {'message': "{} added".format(request.POST.get('to_user'))}
+        return HttpResponse(json.dumps(data), content_type='application/json')
+    else:
+        raise Http404
+
+
+@csrf_exempt
+def cancel_fr(request):
+    if request.is_ajax() and request.POST:
+        from_user = User.objects.get(pk=request.POST.get('from_user'))
+        to_user = User.objects.get(pk=request.POST.get('to_user'))
+
+        FriendshipRequest.objects.get(from_user=from_user, to_user=to_user).delete()
+        data = {'message': "{} friendship request deleted".format(request.POST.get('to_user'))}
         return HttpResponse(json.dumps(data), content_type='application/json')
     else:
         raise Http404
@@ -26,13 +39,13 @@ def handle_request(request):
             to_user = User.objects.get(pk=request.POST.get('to_user'))
             from_user = User.objects.get(pk=request.POST.get('from_user'))
 
-            friend_request = FriendshipRequest.objects.get(from_user=from_user, to_user=to_user)
+            friendship_request = FriendshipRequest.objects.get(from_user=from_user, to_user=to_user)
 
             ans = 'accepted'
             if request.POST.get('result') == 'true':
-                friend_request.accept()
+                friendship_request.accept()
             else:
-                friend_request.reject()
+                friendship_request.reject()
                 FriendshipRequest.objects.get(from_user=from_user, to_user=to_user).delete()
                 ans = "rejected"
 
--- VideoChatProject/videochat/templatetags/videochat_extras.py
+++ VideoChatProject/videochat/templatetags/videochat_extras.py
@@ -1,4 +1,7 @@
 from django.template import Library
+from django.contrib.auth.models import User
+from friendship.models import Friend
+from friendship.models import FriendshipRequest
 
 register = Library()
 
@@ -14,3 +17,24 @@ def get_tuples(value, arg):
         lst = lst[size:]
     return res
 
+
+@register.filter
+def check_not_friends(value, arg):
+    current_user_pk = value
+    other_user_pk = arg
+
+    current_user = User.objects.get(pk=current_user_pk)
+    other_user = User.objects.get(pk=other_user_pk)
+
+    return other_user not in Friend.objects.friends(current_user)
+
+
+@register.filter
+def check_request_sent(value, arg):
+    current_user_pk = value
+    other_user_pk = arg
+
+    current_user = User.objects.get(pk=current_user_pk)
+    other_user = User.objects.get(pk=other_user_pk)
+
+    return FriendshipRequest.objects.filter(from_user=current_user, to_user=other_user)
